fd91c7f364ac5c47b57bf4fe50882c7b841f8193
Alexis Castellanos <alexiscast@Alexiss-MacBook-Pro-2.local>
2023-09-19 02:34:37 -0700

    add icons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { View, Text, StyleSheet, Alert } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
 
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
@@ -69,14 +70,14 @@ const GameScreen = ({ userNumber, onGameOver }) => {
 						<PrimaryButton
 							onPress={() => nextGuessHandler("lower")}
 						>
-							-
+              <Ionicons name="md-remove" size={24} color="white" />
 						</PrimaryButton>
 					</View>
 					<View style={styles.buttonContainer}>
 						<PrimaryButton
 							onPress={() => nextGuessHandler("greater")}
 						>
-							+
+            <Ionicons name="md-add" size={24} color="white" />
 						</PrimaryButton>
 					</View>
 				</View>
