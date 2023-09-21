894abfcf205d82756db77eb024e28bebee588d1e
Alexis Castellanos <alexiscast@Alexiss-MacBook-Pro-2.local>
2023-09-21 14:35:07 -0700

    add platform specific code with Platform API

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 
 const Title = ({ children }) => {
 	return <Text style={styles.title}>{children}</Text>;
@@ -9,10 +9,11 @@ export default Title;
 const styles = StyleSheet.create({
 	title: {
 		fontFamily: "open-sans-bold",
-		fontSize: 24,
+		// fontSize: 24,
 		color: "white",
 		textAlign: "center",
-		borderWidth: 2,
+		// borderWidth: Platform.OS === "android" ? 2 : 0,
+		borderWidth: Platform.select({ ios: 0, android: 2 }),
 		borderColor: "white",
 		padding: 12,
 		maxWidth: "80%",
